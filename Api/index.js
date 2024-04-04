b1df194712e1a70f5739ba2645a4417e56924ae0
Ankit Kumar <132119585+Ankit060607@users.noreply.github.com>
2024-04-04 14:30:45 +0530

    this is my new commit

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
-
+import userroutes from"./routes/userroutes.js";
+import mongoose from 'mongoose';
+mongoose.connect('mongodb+srv://8077151685ankit:ankit321@mern.yvj3f5s.mongodb.net/mern?retryWrites=true&w=majority').then(
+    () =>{
+        console.log("mongodb is connected");
+    }).catch((err) =>{
+        console.log(err);
+    });
 const app = express();
 
 app.listen(3000, ()=>{
     console.log("server run port 3000 !!");
 });
+
+app.use('/Api/user',userroutes);
